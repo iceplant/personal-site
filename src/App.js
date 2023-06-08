ee826180cfb85b16d24d50770b638e097a931a69
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2023-06-08 17:01:29 +0200

    removes borders and irrelevant projects

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-
-import React, { useState } from 'react';
-import BlogWebsite from './BlogWebsite';
-import DanceWebsite from './Dance';
-import DevWebsite from './DevWebsite';
-import PlantShopWebsite from './PlantShop/PlantShopWebsite';
-
+import React, { useState } from "react";
+import BlogWebsite from "./BlogWebsite";
+import DanceWebsite from "./Dance";
+import DevWebsite from "./DevWebsite";
+import PlantShopWebsite from "./PlantShop/PlantShopWebsite";
 
 function App() {
-  const [page, setPage] = useState('Dev');
-  const views = {'Dev' : (<DevWebsite setPage={setPage}/>), 
-                'Blog' : (<BlogWebsite setPage={setPage}/>),
-                'Dance' : (<DanceWebsite setPage={setPage}/>),
-                'PlantShop' : (<PlantShopWebsite setPage={setPage}/>),
-                };
+  const [page, setPage] = useState("Dev");
+  const views = {
+    Dev: <DevWebsite setPage={setPage} />,
+    Blog: <BlogWebsite setPage={setPage} />,
+    Dance: <DanceWebsite setPage={setPage} />,
+    PlantShop: <PlantShopWebsite setPage={setPage} />,
+  };
   return views[page];
 }
 
